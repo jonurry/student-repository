a3732883dfd01655364a1ea88fb1c0e8137a679e
Jon Urry <jon@urry.me>
2018-08-21 17:30:31 +0100

    Refactoring methods

--- directory.rb
+++ directory.rb
@@ -1,28 +1,28 @@
+def get_input(prompt)
+  puts prompt
+  gets.chomp
+end
+
 def input_students
   puts "Please enter the names and details of the students"
   puts "To finish, just hit return twice at the name prompt"
   # creates an empty array
   students = []
   # get the first name
-  puts "Enter student's name"
-  name = gets.chomp
+  name = get_input("Enter student's name")
   # while the name is not empty, repeat this code
   while !name.empty? do
     # get the student's country of birth
-    puts "Which country were they born in?"
-    country = gets.chomp
+    country = get_input("Which country were they born in?")
     # get the student's height
-    puts "How high are they in metres? (e.g. 1.75)"
-    height = gets.chomp
+    height = get_input("How high are they in metres? (e.g. 1.75)")
     # get the student's hobbies
-    puts "What are their hobbies? (e.g. tennis, football, art)"
-    hobbies = gets.chomp
+    hobbies = get_input("What are their hobbies? (e.g. tennis, football, art)")
     # add the student hash to the array
     students << {name: name, cohort: :november, country: country, height: height, hobbies: hobbies.split(", ")}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    puts "Enter the next student's name (or hit return to finish)"
-    name = gets.chomp
+    name = get_input("Enter the next student's name (or hit return to finish)")
   end
   # return the array of students
   students
@@ -34,30 +34,35 @@ def header
             "-------------------------------"]
 end
 
-def generate_student_list(students)
+def footer(names)
+  "Overall, we have #{names.count} great students"
+end
+
+def numbered_list(students)
   student_list = []
   counter = 0
   while counter < students.length do
     student = students[counter]
     counter += 1
-    student_list << "#{counter}. #{student[:name]} - #{student[:country]} (#{student[:cohort]} cohort, #{student[:height]}m, likes #{student[:hobbies].join(", ")})"
+    list_item = "#{counter}. #{student[:name]}"
+    list_item << " - #{student[:country]}"
+    list_item << " (#{student[:cohort]} cohort,"
+    list_item << " #{student[:height]}m,"
+    list_item << " likes #{student[:hobbies].join(", ")})"
+    student_list << list_item
   end
   student_list
 end
 
 def centre_contents(students)
-  centred_output = []
+  centred_content = []
   # work out the longest string in the array
   max_length = students.max { |a, b| a.length <=> b.length }.length
   # centre all strings based on the longest string
   students.each do |student|
-    centred_output << student.center(max_length)
+    centred_content << student.center(max_length)
   end
-  centred_output
-end
-
-def footer(names)
-  "Overall, we have #{names.count} great students"
+  centred_content
 end
 
 def filter_students_by_first_letter(students, letter)
@@ -70,10 +75,10 @@ def filter_students_by_length_of_name(students, length)
   students.select { |student| student[:name].length < length }
 end
 
-def print_students(students)
+def print_student_list(students)
   student_list = []
   student_list << header
-  student_list << generate_student_list(students)
+  student_list << numbered_list(students)
   student_list << footer(students)
   student_list.flatten!
   puts centre_contents(student_list)
@@ -83,4 +88,4 @@ end
 students = input_students
 students = filter_students_by_first_letter(students, "J")
 students = filter_students_by_length_of_name(students, 12)
-print_students(students)
+print_student_list(students)
