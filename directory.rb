b50ca5b96c890df47dd2d5c50f4828772cfbc813
Jon Urry <jon@urry.me>
2018-08-21 15:24:42 +0100

    Centre content on output

--- directory.rb
+++ directory.rb
@@ -28,22 +28,36 @@ def input_students
   students
 end
 
-def print_header
-  puts "The students of Villain Academy"
-  puts "-------------------------------"
+def header
+  # returns an array containing the header contents
+  header = ["The students of Villain Academy",
+            "-------------------------------"]
 end
 
-def print(students)
+def generate_student_list(students)
+  student_list = []
   counter = 0
   while counter < students.length do
     student = students[counter]
     counter += 1
-    puts "#{counter}. #{student[:name]} - #{student[:country]} (#{student[:cohort]} cohort, #{student[:height]}m, likes #{student[:hobbies].join(", ")})"
+    student_list << "#{counter}. #{student[:name]} - #{student[:country]} (#{student[:cohort]} cohort, #{student[:height]}m, likes #{student[:hobbies].join(", ")})"
   end
+  student_list
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def centre_contents(students)
+  centred_output = []
+  # work out the longest string in the array
+  max_length = students.max { |a, b| a.length <=> b.length }.length
+  # centre all strings based on the longest string
+  students.each do |student|
+    centred_output << student.center(max_length)
+  end
+  centred_output
+end
+
+def footer(names)
+  "Overall, we have #{names.count} great students"
 end
 
 def filter_students_by_first_letter(students, letter)
@@ -56,10 +70,17 @@ def filter_students_by_length_of_name(students, length)
   students.select { |student| student[:name].length < length }
 end
 
+def print_students(students)
+  student_list = []
+  student_list << header
+  student_list << generate_student_list(students)
+  student_list << footer(students)
+  student_list.flatten!
+  puts centre_contents(student_list)
+end
+
 # nothing happens until we call the methods
 students = input_students
 students = filter_students_by_first_letter(students, "J")
 students = filter_students_by_length_of_name(students, 12)
-print_header
-print(students)
-print_footer(students)
+print_students(students)
